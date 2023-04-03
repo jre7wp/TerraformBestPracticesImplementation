10cf6ed7442b58198f1511782afb3e1647a53bb9
jesse-r-evans <93012578+jesse-r-evans@users.noreply.github.com>
2023-04-03 13:17:44 -0500

    Made edits to the provider file

--- provider.tf
+++ provider.tf
@@ -2,5 +2,5 @@ provider "google" {
     project = gclitestprj
     region = "us-central1"
     zone = "us-central1-a"
-    credentials = "C:\Users\Jesse\OneDrive\"Terraform working stuff"/gclitestprj-41e6024c9961.json" 
+    credentials = "./gclitestprj-41e6024c9961.json"
 }
