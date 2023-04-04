3285fcdcf70f6026ef8933b2e3b72d8a353365eb
jesse-r-evans <93012578+jesse-r-evans@users.noreply.github.com>
2023-04-04 14:00:18 -0500

    Wrote main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,22 @@
+resource "google_compute_instance" "this"{
+    name = var.server_name
+    machine_type = var.machine_type
+    zone = var.zone
 
+    boot_disk {
+      initialize_params {
+        image = "ubuntu-1804-bionic-v20230324"
+      }
+    }
+
+    network_interface {
+      network = "default"
+      access_config {
+        
+      }
+    }
+
+    metadata_startup_script = file("startup.sh")
+    tags = ["http-server"]
+
+}
--- startup.sh
+++ startup.sh
@@ -1,4 +1,3 @@
 !# /bin/bash
 apt update
 apt install apache2
-DD_API_KEY=7d772296553bfc305a5c047baea5b770 DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
